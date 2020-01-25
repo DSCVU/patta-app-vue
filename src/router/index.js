b9e1ec3a8ed012cdde3007003d473dba1267ee2a
Sulman Baig <sulmanweb@gmail.com>
2020-01-25 19:19:26 +0500

    Adding Sign In page

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import SignUp from "../views/SignUp.vue";
+import SignIn from "../views/SignIn.vue";
 
 Vue.use(VueRouter);
 
@@ -15,6 +16,11 @@ const routes = [
     path: "/sign_up",
     name: "sign_up",
     component: SignUp
+  },
+  {
+    path: "/sign_in",
+    name: "sign_in",
+    component: SignIn
   }
   // {
   //   path: "/about",
